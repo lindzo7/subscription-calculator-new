df5484c76901150ac85599da6d4849667142f3fd
Lindsey Jarrett <lindsey.jarrett7@gmail.com>
2023-10-02 14:23:26 -0400

    added function to calculate cost & added result variable

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 var subTypeElement = document.querySelector("#subscription");
 var subDurationElement = document.querySelector("#months");
+var result = document.querySelector(".result");
 var subType = "basic";
 var subDuration = 1;
 
@@ -12,3 +13,14 @@ subDurationElement.addEventListener("change", function (e) {
     subDuration = Number(e.target.value);
     console.log(subDuration);
 });
+
+var updateSubscriptionDiv = function () {
+    var monthlyCost = 5;
+    if (subType = "standard") {
+        monthlyCost = 7;
+    } else if (subType = "premium") {
+        monthlyCost = 10;
+    }
+    var total = subDuration * monthlyCost;
+    result.innerText = `You have chosen a ${subDuration} month ${subType} plan for $${total}.`;
+}
