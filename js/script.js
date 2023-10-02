bb63212aeb85b74dfe11f180ca7412e6873372d4
Lindsey Jarrett <lindsey.jarrett7@gmail.com>
2023-10-02 14:40:07 -0400

    added code to call function, fixed typos

--- js/script.js
+++ js/script.js
@@ -6,21 +6,21 @@ var subDuration = 1;
 
 subTypeElement.addEventListener("change", function (e) {
     subType = e.target.value;
-    console.log(subType);
+    updateSubscriptionDiv();
 });
 
 subDurationElement.addEventListener("change", function (e) {
     subDuration = Number(e.target.value);
-    console.log(subDuration);
+    updateSubscriptionDiv();
 });
 
 var updateSubscriptionDiv = function () {
     var monthlyCost = 5;
-    if (subType = "standard") {
+    if (subType === "standard") {
         monthlyCost = 7;
-    } else if (subType = "premium") {
+    } else if (subType === "premium") {
         monthlyCost = 10;
     }
     var total = subDuration * monthlyCost;
     result.innerText = `You have chosen a ${subDuration} month ${subType} plan for $${total}.`;
-}
+};
